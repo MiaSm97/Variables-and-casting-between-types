a8179c8784cd0b051187cd7bffb0ab6639154717
Mia Smaldone <eufosm@libero.it>
2022-12-19 15:16:02 +0100

    Esercizio #2

---
+++ src/main/kotlin/Start.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val number: Long = 43567
+    val number2 = 457.56
+    val number3: Short = 34
+    val sum = number + number2 + number3
+    println("Sum: $sum")
+
+}
+/*Declare 3 different variables with number types (e.g. long, double and short),
+calculate their sum and print it using string templates*/
+
